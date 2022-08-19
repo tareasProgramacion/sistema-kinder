f60ddef756a21902115cc2d11f92624aac6eb2d3
tareasProgramacion <107954673+tareasProgramacion@users.noreply.github.com>
2022-08-19 16:47:26 -0500

    panel administracion - docente - curso

--- app.py
+++ app.py
@@ -11,15 +11,19 @@ administrador = Administrador()
 @app.route('/', methods=['GET', 'POST'])
 def login():
   if request.method == 'GET':
+    if 'usuario' in session:
+      if session['usuario']['rol'] == 'administrador':
+        return redirect('/admin')
     return render_template('login.html')
   try:
     usuario = administrador.login(request.form['email'], request.form['password'])
+    usuario['_id'] = str(usuario['_id'])
     session['usuario'] = usuario
     if usuario['rol'] == 'administrador':
-      return 'Panel de administrador'
+      return redirect('/admin')
     return 'Panel de docente'
   except Exception as error:
-    return render_template('login.html',error=error.__str__())
+    return render_template('login.html',data={'error':error.__str__()})
 
 @app.route('/registro', methods=['GET','POST'])
 def registro():
@@ -33,13 +37,92 @@ def registro():
     )
     return redirect('/')
   except Exception as error:
-    return render_template('registro.html',error = error.__str__())
+    return render_template('registro.html',data={'error':error.__str__()})
 
 
 @app.route('/salir',methods=['GET'])
 def salir():
   session.pop('usuario',None)
+  session.pop('estudiante',None)
   return redirect('/')
+
+
+@app.route('/admin',methods=['GET'])
+def admin():
+  if administradorLogueado() == False:
+    return redirect('/')
+
+  return render_template('panel_admin.html')
+
+
+@app.route('/admin/curso',methods=['GET','POST'])
+def adminCurso():
+  if administradorLogueado() == False:
+    return redirect('/')
+
+  if request.method == 'POST':
+    administrador.agregarCurso(request.form['nombre'])
+  cursos = administrador.obtenerCursos()
+  return render_template('panel_admin_curso.html',data={'cursos':cursos})
+  
+
+
+@app.route('/admin/curso/eliminar/<id>',methods=['GET'])
+def adminCursoEliminar(id):
+  if administradorLogueado() == False:
+    return redirect('/')
+
+  administrador.eliminarCurso(id)
+  return redirect('/admin/curso')
+  
+  
+@app.route('/admin/curso/actualizar/<id>',methods=['GET','POST'])
+def adminCursoActualizar(id):
+  if administradorLogueado() == False:
+    return redirect('/')
+
+  data = {}
+  if request.method == 'GET':
+    data['curso'] = administrador.obtenerCurso(id)
+    data['cursos'] = administrador.obtenerCursos()
+    return render_template('panel_admin_curso.html',data=data)
+  else:
+    administrador.actualizarCurso(id,request.form['nombre'])
+    return redirect('/admin/curso')
+
+  
+@app.route('/admin/docente',methods=['GET','POST'])
+def adminDocente():
+  if administradorLogueado() == False:
+    return redirect('/')
+
+  data = {}
+  if request.method == 'POST':
+    try:
+      administrador.agregarDocente(request.form['nombre'], request.form['email'], request.form['password'])
+    except Exception as error:
+      data['error'] = error.__str__()
+
+  data['docentes'] = administrador.obtenerDocentes()
+  return render_template('panel_admin_docente.html',data=data)
+
+@app.route('/admin/docente/eliminar/<id>',methods=['GET'])
+def adminDocenteEliminar(id):
+  if administradorLogueado() == False:
+    return redirect('/')
+
+  administrador.eliminarDocente(id)
+  return redirect('/admin/docente')
+
+
+
+
+def administradorLogueado():
+  if 'usuario' not in session:
+    return False
+  if session['usuario']['rol'] != 'administrador':
+    return False
+  return True
 
 if __name__ == "__main__":
   app.run(debug=True, port=5000)
--- modelos.py
+++ modelos.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 mongo = MongoClient("mongodb+srv://admin:admin@cluster0.zk7c2.mongodb.net/dbkinder?retryWrites=true&w=majority")
 
@@ -45,4 +46,56 @@ class Administrador:
       "rol": "administrador"
     })
 
+  def obtenerCursos(self):
+    lista = []
+    for i in mongo.dbkinder.cursos.find():
+      i['_id'] = str(i['_id'])
+      lista.append(i)
+    return lista
 
+  def obtenerCurso(self, id):
+    return mongo.dbkinder.cursos.find_one({'_id':ObjectId(id)})
+
+  def agregarCurso(self, nombre:str):
+    return mongo.dbkinder.cursos.insert_one({'nombre':nombre.capitalize()})
+
+  def actualizarCurso(self, id:str, nombre:str):
+    return mongo.dbkinder.cursos.update_one({'_id':ObjectId(id)},{'$set':{'nombre':nombre.capitalize()}})
+  
+  def eliminarCurso(self, id:str):
+    return mongo.dbkinder.cursos.delete_one({'_id':ObjectId(id)})
+  
+
+  def obtenerDocentes(self):
+    lista = []
+    for i in mongo.dbkinder.usuarios.find({'rol':'docente'}):
+      i['_id'] = str(i['_id'])
+      lista.append(i)
+      
+    return lista
+
+  def agregarDocente(self, nombre, email, password):
+    existeEmail = mongo.dbkinder.usuarios.find_one({'email':email})
+    if existeEmail:
+      raise Exception('El email ya está registrado!')
+
+    if len(password) < 8:
+      raise Exception('El password debe tener al menos 8 dígitos')
+    
+    docente = {'nombre':nombre,'email':email,'password':password,'rol':'docente'}
+    return mongo.dbkinder.usuarios.insert_one(docente)
+
+  def eliminarDocente(self, id:str):
+    return mongo.dbkinder.usuarios.delete_one({'_id':ObjectId(id)})
+
+  def obtenerParalelos(self):
+    lista = []
+    for i in mongo.dbkinder.paralelos.find():
+      i['_id'] = str(i['_id'])
+      i['_curso'] = str(i['_curso'])
+      i['_docente'] = str(i['_docente'])
+      lista.append(i)
+
+    return lista
+
+  
